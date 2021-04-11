9e441a3bcd4a12b321e3abef3a07e97424e05c14
zachary <zhuba@my.wctc.edu>
2021-04-10 19:01:32 -0500

    PokerTest change

--- src/edu/wctc/test/PokerTest.java
+++ src/edu/wctc/test/PokerTest.java
@@ -4,6 +4,7 @@ import edu.wctc.Card;
 import edu.wctc.Deck;
 import edu.wctc.Poker;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
